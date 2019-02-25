4a1dc1d46d73f0d8bfd88958c8185679223cf263
Charlton Groves <charltongroves@gmail.com>
2019-02-25 11:04:44 +1000

    FIX: spaces in contact

--- src/js/stores/ContactInfo.js
+++ src/js/stores/ContactInfo.js
@@ -1,11 +1,11 @@
 export const contact = {
   about: [
     "Hi, I’m Abbie",
-    "I’m a visual communication designer from Brisbane, Australia.",
-    "I strive to apply creative design thinking and my interdisciplinary knowledge to all projects.",
-    "I’ve always loved learning and creating.Throughout my schooling I was always engaging with the arts.I performed in school musicals, played the clarinet and piano and performed in an indie rock band.Growing up in a small country town(Emerald, Central Queensland), keeping myself entertained through art was vital.",
-    "Today, my hobbies include interacting with the music scene(fav bands are Pond, Unknown Mortal Orchestra), creating illustrative works and constantly trying to immerse myself in new ideas, mediums and culture.",
-    "I’m driven by the opportunity to learn and discover more and using that acquired knowledge to inform my design and illustration projects.",
+    "I’m a visual communication designer from Brisbane, Australia. ",
+    "I strive to apply creative design thinking and my interdisciplinary knowledge to all projects. ",
+    "I’ve always loved learning and creating. Throughout my schooling I was always engaging with the arts. I performed in school musicals, played the clarinet and piano and performed in an indie rock band. Growing up in a small country town(Emerald, Central Queensland), keeping myself entertained through art was vital. ",
+    "Today, my hobbies include interacting with the music scene(fav bands are Pond, Unknown Mortal Orchestra), creating illustrative works and constantly trying to immerse myself in new ideas, mediums and culture. ",
+    "I’m driven by the opportunity to learn and discover more and using that acquired knowledge to inform my design and illustration projects. ",
     "Commission inquiries at ongheen@gmail.com",
   ],
   resumeLabel: "Find my resume here",
